283bf1f2cac70cb6200c1481bce57432251ad4ba
saeedeProfessional <worksaeede@gmail.com>
2021-09-22 14:18:27 +0330

    updated file upload handler

--- js/main.js
+++ js/main.js
@@ -2,25 +2,33 @@ console.log("init");
 
 const uploadResum = (e) => {
   const label = document.getElementById("resume-file-label");
+  const labelText = document.getElementById("resume-label-text");
+  const fileInput = document.getElementById("resume-file");
+  const warningElem = document.getElementById("resum-warn");
+  const infoElem = document.getElementById("resum-info");
+
   if (e.target.files.length > 0) {
-    console.log("!file", e.target.files[0]);
-    var text = document.createTextNode(e.target.files[0].name);
-    label.appendChild(text);
-
-    if (e.target.files[0].size > 10) {
+    if (e.target.files[0].size > 1000000) {
+      fileInput.value = "";
       label.classList.add("warning-file");
-
-      console.log("!file2", e.target.files[0]);
+      label.classList.remove("active-file-x");
+      warningElem.style.display = "block";
+      infoElem.style.display = "none";
     } else {
       label.classList.add("active-file-x");
+      label.classList.remove("warning-file");
+      warningElem.style.display = "none";
+      infoElem.style.display = "block";
     }
+
+    const name = e.target.files[0]?.name;
+    labelText.textContent = name ? name : "";
   } else {
-    label.classList.remove("active-file-x");
-    label.classList.remove("warning-file");
+    label.classList.remove("active-file-x", "warning-file");
   }
 };
 
-const uploadBtnHandler = (e) => {
+const resumeBtnClickHandler = (e) => {
   e.preventDefault();
   const file_element = document.getElementById("resume-file");
   file_element.click();
