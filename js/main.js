aac2d40846c08635918b4d4c501ccb83b33bcdd2
saeedeProfessional <worksaeede@gmail.com>
2021-09-22 13:25:03 +0330

    added warning-file class

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ console.log("init");
 const uploadResum = (e) => {
   const label = document.getElementById("resume-file-label");
   if (e.target.files.length > 0) {
-    // console.log("!!!file", e.target.files[0]);
+    console.log("!file", e.target.files[0]);
     var text = document.createTextNode(e.target.files[0].name);
     label.appendChild(text);
-    label.classList.add("active-file-x");
+
+    if (e.target.files[0].size > 10) {
+      label.classList.add("warning-file");
+
+      console.log("!file2", e.target.files[0]);
+    } else {
+      label.classList.add("active-file-x");
+    }
   } else {
     label.classList.remove("active-file-x");
+    label.classList.remove("warning-file");
   }
 };
 
