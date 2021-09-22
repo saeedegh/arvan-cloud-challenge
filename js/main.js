7de6d894b76feae15e92d0ff44c7ab92c9dcbb8a
saeedeProfessional <worksaeede@gmail.com>
2021-09-22 12:30:21 +0330

    updated file actions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("hello");
+console.log("init");
+
+const uploadResum = (e) => {
+  const label = document.getElementById("resume-file-label");
+  if (e.target.files.length > 0) {
+    // console.log("!!!file", e.target.files[0]);
+    var text = document.createTextNode(e.target.files[0].name);
+    label.appendChild(text);
+  }
+};
+
+const uploadBtnHandler = (e) => {
+  e.preventDefault();
+  const file_element = document.getElementById("resume-file");
+  file_element.click();
+};
