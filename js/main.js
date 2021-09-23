64651bbe6a753653123ed067142a8f428ac10f36
saeedeProfessional <worksaeede@gmail.com>
2021-09-23 18:44:55 +0330

    added form submit

--- js/main.js
+++ js/main.js
@@ -75,3 +75,36 @@ const uploadHandler = (e, fileInputId) => {
   file_element.click();
 };
 //
+
+function checkForm(form) {
+  console.log(form);
+  // validation fails if the input is blank
+  // if (form.inputfield.value == "") {
+  //   alert("Error: Input is empty!");
+  //   form.inputfield.focus();
+  //   return false;
+  // }
+
+  // // regular expression to match only alphanumeric characters and spaces
+  // var re = /^[\w ]+$/;
+
+  // // validation fails if the input doesn't match our regular expression
+  // if (!re.test(form.inputfield.value)) {
+  //   alert("Error: Input contains invalid characters!");
+  //   form.inputfield.focus();
+  //   return false;
+  // }
+
+  // // validation was successful
+  // return true;
+}
+
+const formSubmitHandler = (event) => {
+  event.preventDefault();
+  const myForm = document.getElementById("myForm");
+  const formData = new FormData(myForm);
+
+  const request = new XMLHttpRequest();
+  request.open("post", "https://coda.io/@saeede-work/test1");
+  request.send(formData);
+};
